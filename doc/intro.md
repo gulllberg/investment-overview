c11685b3dcbd97141396c48a9bbb7d8c95a784a7
Daniel Gullberg <daniel.gullberg@evry.com>
2017-08-19 10:36:44 +0200

    Generated files

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to investment-overview
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/investment_overview/core_test.clj
@@ -0,0 +1,7 @@
+(ns investment-overview.core-test
+  (:require [clojure.test :refer :all]
+            [investment-overview.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
